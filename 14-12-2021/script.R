686e60c33c747ecfe7226c936a13649e718609eb
nikdudaev <44577854+nikdudaev@users.noreply.github.com>
2021-12-17 22:19:21 +0100

    Update script.R

--- 14-12-2021/script.R
+++ 14-12-2021/script.R
@@ -1,3 +1,5 @@
+options(scipen = 999)
+
 # Get the Data
 
 # Read in with tidytuesdayR package 
@@ -9,8 +11,35 @@
 tuesdata <- tidytuesdayR::tt_load('2021-12-14')
 
 library(tidyverse)
+library(tidytext)
 
 lyrics <- tuesdata$lyrics
 related_artists <- tuesdata$related_artists
 studio_album_tracks <- tuesdata$studio_album_tracks
 rm(tuesdata)
+
+studio_album_tracks %>% ggplot(aes(x = danceability, y = energy)) + geom_point()
+studio_album_tracks %>% ggplot(aes(x = danceability, y = loudness)) + geom_point()
+studio_album_tracks %>% ggplot(aes(x = danceability, y = speechiness)) + geom_point()
+studio_album_tracks %>% ggplot(aes(x = danceability, y = valence)) + geom_point()
+
+# Tokenizing lyrics
+lyrics_tokenized <- unnest_tokens(lyrics, word, line, drop = FALSE)
+
+# Removing Stop Words
+tidy_lyrics <- lyrics_tokenized %>% anti_join(stop_words)
+
+common_words <- tidy_lyrics %>% count(word, sort = TRUE)
+common_words %>% filter(n > 50) %>% mutate(word = reorder(word, n)) %>% ggplot(aes(x = word, y = n)) + geom_col() + xlab(NULL) + coord_flip()
+
+nrcjoy <- get_sentiments("nrc") %>% filter(sentiment == "joy")
+
+tidy_lyrics %>%
+  inner_join(nrcjoy) %>%
+  count(word, sort = TRUE)
+
+nrcsadness <- get_sentiments("nrc") %>% filter(sentiment == "sadness")
+
+tidy_lyrics %>%
+  inner_join(nrcsadness) %>%
+  count(word, sort = TRUE)
